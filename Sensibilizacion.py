6507f17e8ac91b88edc3f7d9b3cadae1615983b0
Julian Velasquez <juliddv1@gmail.com>
2023-02-10 13:45:42 -0500

    First Example

---
+++ Sensibilizacion.py
@@ -0,0 +1,9 @@
+class Vehiculo:
+    cantidad = 0
+
+    def __init__(self):
+        cantidad = self.cantidad
+
+    def pasajeros(self):
+        if self.cantidad > 5:
+            print("Sobrecupo")
